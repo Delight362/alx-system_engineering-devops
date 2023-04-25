8969bb8b2ee78518fa9b376ae9990e9c8d67c495
Delight362 <oparauredu@gmail.com>
2023-04-25 05:04:22 +0100

    API

--- 0x15-api/00-gather_data_from_an_API.py
+++ 0x15-api/00-gather_data_from_an_API.py
@@ -1,3 +1,6 @@
+
+ 
+
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
 import requests
@@ -11,4 +14,3 @@ if __name__ == "__main__":
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
